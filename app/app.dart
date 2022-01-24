11ffcbda0d515ea1571f540ce859cf8e436346fb
nassimgc <nassim.bougtib@hotmail.com>
2022-01-24 16:26:34 +0100

    ajout verification age

--- app/app.dart
+++ app/app.dart
@@ -1,5 +1,20 @@
 
+// importing dart:io file
+import 'dart:io';
 main() {
-    var img = { 'link': 'http' };
-    print('Hello: ' + img['link'].toString());
+    // Asking for favourite number
+    print("Saisissez votre âge svp");
+ 
+   // Scanning number
+    int? age = int.parse(stdin.readLineSync()!);
+   // Here ? and ! are for null safety
+ 
+    // Printing that number
+    print("Vous avez $age ans");
+
+    if (age >= 18) {
+      print('Vous êtes majeur');
+    } else {
+      print('Vous êtes mineur');
+    }
 }
