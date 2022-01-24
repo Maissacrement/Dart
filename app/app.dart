068149448e17adac10d0a7df356f85efe697c8de
Clement Dimanchin <clementdimanchin@free.fr>
2022-01-24 15:55:19 +0100

    exo 1

--- app/app.dart
+++ app/app.dart
@@ -1,5 +1,9 @@
 
 main() {
-    var img = { 'link': 'http' };
-    print('Hello: ' + img['link'].toString());
+    var ?Int age = stdin.readLineSync();
+    if(age >= 18){
+        print('Majeur');
+    }else{
+        print('Mineur');
+    }
 }
